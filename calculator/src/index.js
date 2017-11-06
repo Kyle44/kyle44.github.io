3e5b43a01e196b759f13a71b7c46c30ce532f2dc
Kyle <kyle.programmer44@gmail.com>
2017-11-06 17:18:02 -0500

    CalculatorButton complete, some buttons on screen

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -7,7 +7,30 @@ var destination = document.querySelector("#container")
   
 ReactDOM.render(
     <div>
-        <CalculatorButton/>
-    </div>,
+    	<div className="row">
+		    <CalculatorButton text="1"/>
+	        <CalculatorButton text="2"/>
+			<CalculatorButton text="3"/>
+			<CalculatorButton text="+"/>
+		</div>
+		<div className="row">
+		    <CalculatorButton text="4"/>
+	        <CalculatorButton text="5"/>
+			<CalculatorButton text="6"/>
+			<CalculatorButton text="-"/>
+		</div>
+		<div className="row">
+		    <CalculatorButton text="7"/>
+	        <CalculatorButton text="8"/>
+			<CalculatorButton text="9"/>
+			<CalculatorButton text="*"/>
+		</div>
+		<div className="row">
+		    <CalculatorButton text="."/>
+	        <CalculatorButton text="0"/>
+			<CalculatorButton text="="/>
+			<CalculatorButton text="/"/>
+		</div>
+	</div>,
     destination
 );
