8c5c15d328ed8b7d3006dcf1e1a1ba02c2a12f8e
Kyle <kyle.primary@gmail.com>
2023-02-26 16:35:05 -0500

    test change for deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import  Carousel  from './components/MyCarousel.js';
 
 function createHorses(){
   const myApps = [
-    {sourceCode: "https://github.com/Kyle44/Sudoku", name: "Sudoku", url: ""},
+    {sourceCode: "https://github.com/Kyle44/Sudoku", name: "Sudoku TEST", url: ""},
     {sourceCode: "https://github.com/Kyle44/Blackjack", name: "Blackjack", url: ""},
     {sourceCode: "https://github.com/Kyle44/TimeTellingForKids", name: "Time Telling for Kids", url: "", videoDemo: "https://www.youtube.com/watch?v=bWtcCHXrQv4&ab_channel=KyleFritz"},
     {sourceCode: "", name: "Prime Numbers", url: ""},
